0a454650d91a69655994f6969ceaf1ab03a0b370
Matheus Franco De Sá Taveira <matheusfstaveira@gmail.com>
2023-04-04 21:38:03 -0300

    Mudança nas hints

--- 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/CadastroEndereco.kt
+++ 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/CadastroEndereco.kt
@@ -2,8 +2,11 @@ package br.com.ForTeethDentalCare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.ForTeethDentalCare.databinding.ActivityMainBinding
 
 class CadastroEndereco : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_endereco)
