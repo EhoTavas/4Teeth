6017f5ae9fdc418ff3ccbe1349cd8b519e2c06f8
Matheus Franco De Sá Taveira <matheusfstaveira@gmail.com>
2023-04-04 21:20:23 -0300

    Layout inicial da tela de cadastro

---
+++ 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/CadastroEndereco.kt
@@ -0,0 +1,11 @@
+package br.com.ForTeethDentalCare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroEndereco : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_endereco)
+    }
+}
--- 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/MainActivity.kt
+++ 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/MainActivity.kt
@@ -1,11 +1,21 @@
 package br.com.ForTeethDentalCare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.ForTeethDentalCare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.icNavegarProx.setOnClickListener {
+           val continuarCadastro = Intent (this, CadastroEndereco::class.java)
+            startActivities(arrayOf(continuarCadastro))
+        }
     }
 }
