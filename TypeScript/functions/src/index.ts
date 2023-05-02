c27a223f6163a3b0d3b88259c871baf061c5008c
Matheus Franco De Sá Taveira <matheusfstaveira@gmail.com>
2023-05-02 18:32:12 -0300

    Implementações firebase e functions

--- TypeScript/functions/src/index.ts
+++ TypeScript/functions/src/index.ts
@@ -1,27 +1,140 @@
+
+/* eslint-disable linebreak-style */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-const app = admin.initializeApp();
-const db = app.firestore();
-const colDentistas = db.collection("Dentista");
+// inicializando o firebase admin
+const firebase = admin.initializeApp();
+    type Dentista = {
+    nome: string,
+    email: string,
+    telefone: string,
+    fcmToken: string | undefined,
+    uid: string,
+  }
 
-export const addDentist = functions
+  /**
+   * Tipo para facilitar o retorno
+   * de qualquer função.
+   * Basta usar esse objeto sempre como
+   * retorno.
+   */
+  type CustomResponse = {
+    status: string | unknown,
+    message: string | unknown,
+    payload: unknown,
+  }
+
+/**
+   * Essa função pura (sem ser cloud function)
+   * verifica se o parametro data contem:
+   * nome, email, telefone e uid (lembrando que
+   * a senha não armazenamos no perfil do firestore).
+   * @param {any} data - objeto data (any).
+   * @return {boolean} - true se tiver dados corretos
+   */
+function hasAccountData(data: Dentista) {
+  if (data.nome != undefined &&
+        data.email != undefined &&
+        data.telefone != undefined &&
+        data.uid != undefined &&
+        data.fcmToken != undefined) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const setUserProfile = functions
   .region("southamerica-east1")
-  .https.onRequest(async (_request, response) => {
-    const dentista = {
-      Nome: "Matheus",
-      Email: "Matheusfstaveira@gmail.com",
-      Telefone: "(19)98235-0750",
-      Endereco1: "Rua São Jorge",
-      Endereco2: "Rua São José",
-      Endereco3: "Rua São Joaquim",
-      MiniCurriculo: "SimSimSimSimSimSalabim",
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context) => {
+    // verificando se o token de depuracao foi fornecido.
+    /*
+      if (context.app == undefined) {
+        throw new functions.https.HttpsError(
+          "failed-precondition",
+          "Erro ao acessar a function sem token do AppCheck.");
+      }*/
+    // inicializar um objeto padrao de resposta já com erro.
+    // será modificado durante o curso.
+    const cResponse: CustomResponse = {
+      status: "ERROR",
+      message: "Dados não fornecidos",
+      payload: undefined,
     };
-    try {
-      const docRef = await colDentistas.add(dentista);
-      response.send("Usuário cadastrado. Referência:" + docRef.id);
-    } catch (e) {
-      functions.logger.error("Erro ao cadastrar usuário.");
-      response.send("Erro ao cadastrar usuário.");
+      // verificar se o objeto dentista foi fornecido
+    const dentista = (data as Dentista);
+    if (hasAccountData(dentista)) {
+      try {
+        const doc = await firebase.firestore()
+          .collection("users")
+          .add(dentista);
+        if (doc.id != undefined) {
+          cResponse.status = "SUCCESS";
+          cResponse.message = "Perfil de dentista inserido";
+          cResponse.payload = JSON.stringify({docId: doc.id});
+        } else {
+          cResponse.status = "ERROR";
+          cResponse.message = "Não foi possível inserir o perfil do dentista.";
+          cResponse.payload = JSON.stringify({errorDetail: "doc.id"});
+        }
+      } catch (e) {
+        let exMessage;
+        if (e instanceof Error) {
+          exMessage = e.message;
+        }
+        functions.logger.error("Erro ao incluir perfil:", dentista.email);
+        functions.logger.error("Exception: ", exMessage);
+        cResponse.status = "ERROR";
+        cResponse.message = "Erro ao incluir usuário - Verificar Logs";
+        cResponse.payload = null;
+      }
+    } else {
+      cResponse.status = "ERROR";
+      cResponse.message = "Perfil faltando informações";
+      cResponse.payload = undefined;
     }
+    return JSON.stringify(cResponse);
   });
+
+export const sendFcmMessage = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context) => {
+    const cResponse: CustomResponse = {
+      status: "ERROR",
+      message: "Dados não fornecidos ou incompletos",
+      payload: undefined,
+    };
+      // enviar uma mensagem para o token que veio.
+    if (data.fcmToken != undefined && data.textContent != undefined) {
+      try {
+        const message = {
+          data: {
+            text: data.textContent,
+          },
+          token: data.fcmToken,
+        };
+        const messageId = await firebase.messaging().send(message);
+        cResponse.status = "SUCCESS";
+        cResponse.message = "Mensagem enviada";
+        cResponse.payload = JSON.stringify({messageId: messageId});
+      } catch (e) {
+        let exMessage;
+        if (e instanceof Error) {
+          exMessage = e.message;
+        }
+        functions.logger.error("Erro ao enviar mensagem");
+        functions.logger.error("Exception: ", exMessage);
+        cResponse.status = "ERROR";
+        cResponse.message = "Erro ao enviar mensagem - Verificar Logs";
+        cResponse.payload = null;
+      }
+    }
+    return JSON.stringify(cResponse);
+  });
+
+
