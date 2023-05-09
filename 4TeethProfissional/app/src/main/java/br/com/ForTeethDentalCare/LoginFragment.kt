10d11b32bf2fbd131b16dfb029fdb64ab5b57381
LuanMagriTarran <104510650+LuanMagriTarran@users.noreply.github.com>
2023-05-09 11:33:07 -0300

    Alterações tela Loren e notificação
    Notificação fica no botão de login
    
    Co-Authored-By: loren sev <80834658+19loren@users.noreply.github.com>

--- 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/LoginFragment.kt
+++ 4TeethProfissional/app/src/main/java/br/com/ForTeethDentalCare/LoginFragment.kt
@@ -2,25 +2,30 @@ package br.com.ForTeethDentalCare
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.ForTeethDentalCare.databinding.FragmentLoginBinding
-import br.com.ForTeethDentalCare.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
+import com.google.gson.GsonBuilder
 
 class LoginFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var functions: FirebaseFunctions
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private val gson = GsonBuilder().enableComplexMapKeySerialization().create()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,28 +52,38 @@ class LoginFragment : Fragment() {
         }
 
         binding.btnSignUp.setOnClickListener {
+            sendMessage(binding.EtEmailLogin.text.toString(),(activity).getFcmToken())
+                .addOnCompleteListener(requireActivity()) { res ->
+                    // conta criada com sucesso.
+                    if(res.result.status == "SUCCESS"){
+                        hideKeyboard()
+                        Snackbar.make(requireView(),"Mensagem Enviada!", Snackbar.LENGTH_LONG).show()
+                        binding.EtEmailLogin.text!!.clear()
+                    }
+                }
             findNavController().navigate(R.id.Login_to_SignUp)
         }
     }
-    /*
-        private fun View.hideKeyboard() {
-            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(windowToken, 0)
-        }
 
-        private fun newLogin() {
-            binding.btnLogin.hideKeyboard()
-            email = binding.EtEmailLogin.text.toString().trim()
-            password = binding.EtPasswordLogin.text.toString().trim()
-
-            if (!email.isNullOrBlank() && !password.isNullOrBlank()) {
-                login(email, password)
-            }
-        }
-    */
     private fun hideKeyboard(){
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
+
+    fun sendMessage(textContent: String, fcmToken: String) : Task<CustomResponse> {
+        val data = hashMapOf(
+            "textContent" to textContent,
+            "fcmToken" to fcmToken
+        )
+        // enviar a mensagem, invocando a function...
+        functions = Firebase.functions("southamerica-east1")
+        return functions.getHttpsCallable("sendFcmMessage")
+            .call(data)
+            .continueWith { task ->
+                val result =
+                    gson.fromJson((task.result?.data as String), CustomResponse::class.java)
+                result
+            }
     }
 
     private fun login(email: String, password: String){
